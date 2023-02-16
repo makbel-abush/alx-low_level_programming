69e68f55029ca90861485f92c5cae526055e06f5
makbel-abush <redietdamtew02@gmail.com>
2023-02-16 12:11:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-vi 4-puts.c  
+#include <stdio.h>
+/**
+ * main . main block 
+ * return:0
+ */
+int main(void)
+{
+	puts ("\""Programming is like building a multilingual puzzle");
+	return(0);
+
+}
