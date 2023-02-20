c5b2dd8a21c41ec8b0611d82fdbeb0f531f23d17
makbel-abush <redietdamtew02@gmail.com>
2023-02-20 15:00:46 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d  and is 0\n", n, num);
 	}
-	else
+	else if (num !=0 && num < 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not  0\n", n, num);
 	}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - Print alpha in lower case
+ * Return: Always (Succsess)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc == 'a'; lc <= z 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
