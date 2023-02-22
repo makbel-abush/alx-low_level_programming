481457373648cf2c736fc2e9700f96bad11ebd95
makbel-abush <redietdamtew02@gmail.com>
2023-02-22 15:24:17 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
-
 /**
- * main - scrip to print _putchr
+ * main - Entr point
  *
- * Return: 0
+ * Return:Always 0 (succsess)
  */
 int main(void)
+
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,3 +17,6 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
+
+}
+
