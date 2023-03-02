808db01543b56ea80c3616d4190ffd0c115b3466
makbel-abush <redietdamtew02@gmail.com>
2023-03-02 02:23:03 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - strings to be concatenated
+ * @dest: pointer results to this
+ * @src: string to dest string
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
